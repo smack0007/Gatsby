944969ffe5d415c00c91ffbfeefa9ac871a29dc6
smack0007 <zachary.snow@gmail.com>
2014-01-29 11:10:55 +0100

    Basic implementation of ProcessorPathFilter.

--- Source/Gatsby/Program.cs
+++ Source/Gatsby/Program.cs
@@ -3,13 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ComponentGlue;
 
 namespace Gatsby
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public Program()
         {
+        }
+
+        public int Run(string[] args)
+        {
+            return 0;
+        }
+
+        public static int Main(string[] args)
+        {
+            ComponentContainer container = new ComponentContainer();
+            container.Bind<IProcessorPathFilter>().To<ProcessorPathFilter>().AsTransient();
+
+            var program = container.Resolve<Program>();
+            return program.Run(args);
         }
     }
 }
