61409de734e928e4cb4167a94049a9573faad6bf
smack0007 <zachary.snow@gmail.com>
2014-02-13 20:21:48 +0100

    Switched to Moonshine for Markdown transformations.

--- Source/Gatsby/MarkdownTransformer.cs
+++ Source/Gatsby/MarkdownTransformer.cs
@@ -1,5 +1,4 @@
-﻿using MarkdownSharp;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,21 +7,14 @@ using System.Threading.Tasks;
 namespace Gatsby
 {
     public class MarkdownTransformer
-    {
-        Markdown markdown;
-
-        public MarkdownTransformer(MarkdownHighlighter highlighter)
+    {       
+        public MarkdownTransformer()
         {
-            this.markdown = new Markdown(new MarkdownOptions()
-            {
-            });
-
-            this.markdown.Highlighter = highlighter;
         }
 
         public string Transform(string input)
         {
-            return this.markdown.Transform(input);
+            return Sundown.MoonShine.Markdownify(input, Sundown.MarkdownExtensions.FencedCode);
         }
     }
 }
