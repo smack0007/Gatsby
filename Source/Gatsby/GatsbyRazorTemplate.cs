936287934367cb4b5d2dd944f7b409943fbabfcc
smack0007 <zachary.snow@gmail.com>
2014-03-04 19:14:28 +0100

    Added a UrlSlug method to RazorTemplates.

--- Source/Gatsby/GatsbyRazorTemplate.cs
+++ Source/Gatsby/GatsbyRazorTemplate.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace Gatsby
@@ -23,6 +24,36 @@ namespace Gatsby
         protected string BaseUrl(string relativeUrl)
         {
             return this.Site.BaseUrl + "/" + relativeUrl;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        /// <remarks>Credit to Joan from StackOverflow: http://stackoverflow.com/questions/2920744/url-slugify-algorithm-in-c </remarks>
+        protected static string UrlSlug(string value)
+        {
+            //First to lower case
+            value = value.ToLowerInvariant();
+
+            //Remove all accents
+            var bytes = Encoding.GetEncoding("Cyrillic").GetBytes(value);
+            value = Encoding.ASCII.GetString(bytes);
+
+            //Replace spaces
+            value = Regex.Replace(value, @"\s", "-", RegexOptions.Compiled);
+
+            //Remove invalid chars
+            value = Regex.Replace(value, @"[^a-z0-9\s-_]", "", RegexOptions.Compiled);
+
+            //Trim dashes from end
+            value = value.Trim('-', '_');
+
+            //Replace double occurences of - or _
+            value = Regex.Replace(value, @"([-_]){2,}", "$1", RegexOptions.Compiled);
+
+            return value;
         }
     }
 }
